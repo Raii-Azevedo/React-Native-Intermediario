5382b86ab37b17b01c50288238a1b9eceb737680
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-06-30 19:34:56 -0300

    Uso do Slider

--- Slider/App.js
+++ Slider/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text, Stylesheet } from 'react-native';
 
+import {Slider} from '@react-native-community/slider';
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            valor:0,
+
+        };
+    }
     render(){
         return(
             <View style={styles.container}>
-                
+                <Slider minimumValue={0} 
+                maximumValue={100} 
+                onValueChange={(valorSelecinado) => this.setState({valor:valorSelecinado})}
+                value={this.state.valor}
+                minimumTrackTintColor="#757"
+                maximumTrackTintColor="#ff0000"/>
+
+                <Text>Você tem {this.state.valor.toFixed(1)} kg</Text>
             </View>
         );
     }
