fc0dec3913683a3c93cfab7afb69948cf790a4c0
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-07-03 07:41:25 -0300

    Uso do Switch

--- Switch/App.js
+++ Switch/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, Stylesheet } from 'react-native';
+import { View, Text, Stylesheet, Switch } from 'react-native';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: false,
+        };
+    }
     render(){
         return(
             <View style={styles.container}>
+                <Switch
+                value = {this.state.status}
+                onValueChange={(valorSwitch) => this.setState({status: valorSwitch})}
+                thumbColor="#f000f0"
+                />
+                <Text style={styles.textSwitch}>{(this.state.status) ? "Ativo" : "Inativo"}</Text>
                 
             </View>
         );
@@ -14,6 +26,11 @@ class App extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+
+    textSwitch:{
+        fontSize: 26,
+        textAlign: "center",
     },
 });
 
